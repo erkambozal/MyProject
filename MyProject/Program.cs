ca17628136e0a81302b40a5835149875c564b171
erkambozal <83507894+erkambozal@users.noreply.github.com>
2022-09-12 17:05:08 +0300

    Giriş

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string kategoriEtiketi = "Kategori";
+int ogrenciSayisi = 3200;
+double faiz = 1.45;
+bool sistemeGirisYapmisMi = false;
+double dolarDun = 7.55;
+double dolarBugun = 7.45;
+
+if (dolarDun > dolarBugun) 
+{
+    Console.WriteLine("Azalış Butonu");
+}
+else if (dolarDun < dolarBugun)
+{
+    Console.WriteLine("Artış Butonu");
+}
+else
+{
+    Console.WriteLine("Değişmedi");
+}
+
+if (sistemeGirisYapmisMi == true)
+{
+    Console.WriteLine("Kullanıcı Ayarları Butonu");
+}
+else if (sistemeGirisYapmisMi == false)
+{
+    Console.WriteLine("Giriş Yap Butonu");
+}
+
 Console.WriteLine(kategoriEtiketi);
+
